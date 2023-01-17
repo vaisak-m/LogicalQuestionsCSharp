9f8a0005c61d3f41f041eb715083a2e2441cc461
vxisxk <vaisak@protonmail.com>
2023-01-17 14:13:11 +0530

    q5

---
+++ 5/5/Program.cs
@@ -0,0 +1,26 @@
+﻿int even = 0, odd = 1;
+double fare;
+Console.WriteLine("Enter total fares: ");
+var total = int.Parse(Console.ReadLine());
+
+double[] arrFare = new double[total*2]; 
+for (int i = 0; i < total; i++)
+{
+    Console.WriteLine("Enter Fare: ", i + 1);
+    fare = double.Parse(Console.ReadLine());
+    if(fare % 2 == 0)
+    {
+        arrFare[even] = fare;
+         even += 2;
+    }
+    else
+    {
+        arrFare[odd] = fare;
+        odd += 2;
+    }
+}
+
+foreach(var arr in arrFare)
+{
+    Console.WriteLine("Order of Fares is : " + arr);
+}
