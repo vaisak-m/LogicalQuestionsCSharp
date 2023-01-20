1f8079869c8070c65fe4146456323c76f71b54dd
VAISAK M <90459333+vaisak-m@users.noreply.github.com>
2023-01-20 12:46:50 +0530

    Add files via upload

---
+++ 13/13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Threading.Channels;
+
+ArrayList flightNumbers = new ArrayList();
+
+int index = 0;
+int count = 0;
+
+while (count == 0)
+{
+    Console.WriteLine("Press 1 to Enter");
+    Console.WriteLine("Press 2 to Quit");
+    int userResponse = int.Parse(Console.ReadLine()!);
+
+    switch (userResponse)
+    {
+        case 1:
+            Console.WriteLine("Enter Flight Number: ");
+            int flightNumber = int.Parse(Console.ReadLine()!);
+            flightNumbers.Add(flightNumber);
+            index++;
+            break;
+
+        case 2:
+            count = 1;
+            break;
+    }
+}
+
+//int i = 1;
+//foreach(int flightNumber in flightNumbers)
+//{
+//    Console.WriteLine(i++  +". Flight Numbers are: " + flightNumber);
+//}
+
+for(int i = 0;i < flightNumbers.Count; i++)
+{
+    Console.Write(flightNumbers[i]+ " ");
+}
+
