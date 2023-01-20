aeb575bb62355a9d9ff9a34e18fd1d7c40c4c7ec
VAISAK M <90459333+vaisak-m@users.noreply.github.com>
2023-01-20 11:54:53 +0530

    Add files via upload

---
+++ 15/15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Enter Number of passengers: " );
+int passengers = int.Parse( Console.ReadLine() );
+
+double[] weights = new double[passengers]; 
+
+for ( int i = 0;i < passengers; i++)
+{
+    Console.WriteLine("Enter Baggage Weights "  + (i + 1) + " :");
+    weights[i] = double.Parse(Console.ReadLine());
+}
+
+foreach(var weight in weights)
+{
+    if(weight <= 23)
+    {
+        Console.WriteLine("No Excess Baggage");
+    }
+    else if(weight > 23)
+    {
+        Console.WriteLine( " Excess Baggage Found. You will be charged " + ((weight - 23) * 15) + " Dollars");
+    }
+}
+
+for (int i = 0; i < passengers; i++)
+{
+    for (int j = i + 1; j < passengers; j++)
+    {
+        if (weights[i] > weights[j])
+        {
+            double sort = weights[i];
+            weights[i] = weights[j];
+            weights[j] = sort;
+        }
+    }
+}
+
+foreach(var weight in weights)
+{
+    Console.WriteLine("Weights are: " + weight);
+}
